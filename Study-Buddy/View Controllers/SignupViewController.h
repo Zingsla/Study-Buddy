acdc8b020cda5802a9d51d1c45329dca34357292
Jacob Franz <franzj@fb.com>
2020-07-13 16:40:25 -0500

    Added sign up functionality

---
+++ Study-Buddy/View Controllers/SignupViewController.h
@@ -0,0 +1,19 @@
+//
+//  SignupViewController.h
+//  Study-Buddy
+//
+//  Created by Jacob Franz on 7/13/20.
+//  Copyright © 2020 Jacob Franz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignupViewController : UIViewController
+
+- (BOOL)allFieldsFilled;
+
+@end
+
+NS_ASSUME_NONNULL_END
