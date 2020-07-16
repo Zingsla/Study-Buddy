a20ac1e896859b47ff8e0adaf1b541ee0e433eda
Jacob Franz <franzj@fb.com>
2020-07-16 10:29:20 -0500

    Added logout functionality

---
+++ Study-Buddy/View Controllers/ProfileViewController.m
@@ -0,0 +1,50 @@
+//
+//  ProfileViewController.m
+//  Study-Buddy
+//
+//  Created by Jacob Franz on 7/15/20.
+//  Copyright © 2020 Jacob Franz. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+#import "LoginViewController.h"
+#import "SceneDelegate.h"
+#import "User.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapLogout:(id)sender {
+    SceneDelegate *myDelegate = (SceneDelegate *) self.view.window.windowScene.delegate;
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    myDelegate.window.rootViewController = loginViewController;
+    
+    [User logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        if (error != nil) {
+            NSLog(@"Error logging out: %@", error.localizedDescription);
+        } else {
+            NSLog(@"Successfully logged out!");
+        }
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
