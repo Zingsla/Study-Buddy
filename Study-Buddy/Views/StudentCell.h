9cab1e7780cbe2ecb280b18f872ec23f67a63336
Jacob Franz <franzj@fb.com>
2020-07-17 17:47:35 -0500

    Added students tableview to course details

---
+++ Study-Buddy/Views/StudentCell.h
@@ -0,0 +1,24 @@
+//
+//  StudentCell.h
+//  Study-Buddy
+//
+//  Created by Jacob Franz on 7/17/20.
+//  Copyright © 2020 Jacob Franz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "User.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StudentCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *majorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yearLabel;
+@property (strong, nonatomic) User *user;
+
+@end
+
+NS_ASSUME_NONNULL_END
