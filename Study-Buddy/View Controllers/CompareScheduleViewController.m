5a868c3263c9743c0fc749fc9037336f7be42c8e
Jacob Franz <franzj@fb.com>
2020-08-03 12:58:09 -0500

    Removed more unnecessary comments

--- Study-Buddy/View Controllers/CompareScheduleViewController.m
+++ Study-Buddy/View Controllers/CompareScheduleViewController.m
@@ -91,14 +91,4 @@ NSString *const kPageViewControllerIdentifier = @"PageViewContoller";
     return 0;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
